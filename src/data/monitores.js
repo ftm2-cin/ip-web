ec875507de9926c80ccc87f3afb5da234880c074
Thiago José Alves de Souza <thijalves@gmail.com>
2022-11-28 16:54:00 -0300

    tutors updated

--- src/data/monitores.js
+++ src/data/monitores.js
@@ -1,96 +1,88 @@
-import murilo from "../assets/img/murilo.jpeg";
-import luquet from "../assets/img/luquet.jpeg";
-import matheus from "../assets/img/matheus.jpeg";
-import jeferson from "../assets/img/jeferson.jpeg";
 import jdsc2 from "../assets/img/jdsc2.jpg";
 import jmbj2 from "../assets/img/jmbj2.jpg";
 import pfbc2 from "../assets/img/pfbc2.jpg";
-import rnfo from "../assets/img/rnfo.jpg";
 import acm from "../assets/img/acm.jpg";
 import maa5 from "../assets/img/maa5.jpg";
 import tjas from "../assets/img/tjas.jpg";
+import alas4 from "../assets/img/alas4.jpeg";
+import ffss from "../assets/img/ffss.jpeg";
+import dlc3 from "../assets/img/dlc3.jpeg";
+import gnmp from "../assets/img/gnmp.jpeg";
 
 const data = [
     {
-        name: "Murilo Barbosa",
-        login: "mbn2",
-        periodo: 4,
-        img: murilo,
-        linkedin: "https://www.linkedin.com/in/murilobn/",
-        github: "https://github.com/mulirow",
-    },
-    {
-        name: "Victoria Luquet",
-        login: "vllst",
-        periodo: 4,
-        img: luquet,
-        linkedin: "https://www.linkedin.com/in/victoria-luquet-8766891a2/",
-        github: "https://github.com/vicluq",
-    },
-    {
-        name: "Matheus Boncsidai",
-        login: "mjbo",
-        periodo: 4,
-        img: matheus,
-        linkedin: "",
-        github: "https://github.com/Mathbonc",
-    },
-    {
-        name: "Jeferson Severino",
-        login: "jsa2",
-        periodo: 4,
-        img: jeferson,
-        linkedin: "https://www.linkedin.com/in/jeferson-severino-959925184/",
-        github: "https://github.com/jefersonseverino",
-    },
-    {
-        name: "Daniel Silva",
-        login: "jdsc2",
-        periodo: 3,
-        img: jdsc2,
-        linkedin: "",
-        github: "",
-    },
-    {
         name: "José Marcondes",
         login: "jmbj2",
-        periodo: 3,
+        periodo: 5,
         img: jmbj2,
         linkedin: "",
-        github: "",
+        github: "https://github.com/mallocaholic",
     },
     {
         name: "Pedro Fernandes",
         login: "pfbc2",
-        periodo: 3,
+        periodo: 5,
         img: pfbc2,
         linkedin: "",
         github: "https://github.com/fernandesbarreto",
     },
     {
-        name: "Renato Nogueira",
-        login: "rnfo",
-        periodo: 4,
-        img: rnfo,
-        linkedin: "",
-        github: "https://github.com/RenatoNogFer",
-    },
-    {
-        name: "Marcela",
+        name: "Marcela Asfora",
         login: "maa5",
-        periodo: 2,
+        periodo: 4,
         img: maa5,
         linkedin: "",
         github: "https://github.com/maa134340",
     },
     {
-        name: "Thiago",
+        name: "Thiago Alves",
         login: "tjas",
-        periodo: 2,
+        periodo: 4,
         img: tjas,
         linkedin: "https://www.linkedin.com/in/thijalves/",
         github: "https://github.com/Thijalves",
     },
+    {
+        name: "Daniel Silva",
+        login: "jdsc2",
+        periodo: 5,
+        img: jdsc2,
+        linkedin: "",
+        github: "",
+    },
+    {
+        name: "Ana Laura",
+        login: "alas4",
+        periodo: 2,
+        img: alas4,
+        linkedin: "https://www.linkedin.com/in/ana-laura-albuquerque-616365258",
+        github: "https://github.com/anaalbsantos",
+    },
+    {
+        name: "Danilo Carvalho",
+        login: "dlc3",
+        periodo: 2,
+        img: dlc3,
+        linkedin: "",
+        github: "https://github.com/DaniloLDCarvalho",
+    },
+    {
+        name: "Felipe santos",
+        login: "ffss",
+        periodo: 2,
+        img: ffss,
+        linkedin: "https://www.linkedin.com/in/felipe-ferreira-da-silva-santos-15aa4624a/",
+        github: "https://github.com/anaalbsantos",
+    },
+    {
+        name: "Gabriel Nogueira",
+        login: "gnmp",
+        periodo: 2,
+        img: gnmp,
+        linkedin: "https://www.linkedin.com/in/gabriel-nogueira-a4849424b/",
+        github: "https://github.com/nogueira04",
+    },
+    
 ];
 
 

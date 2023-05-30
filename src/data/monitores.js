192e9f53e42918c08da0022780195efe1dce60ba
thiago <thijalves@gmail.com>
2023-05-29 21:51:22 -0300

    renovando monitores :)

--- src/data/monitores.js
+++ src/data/monitores.js
@@ -1,6 +1,3 @@
-import jdsc2 from "../assets/img/jdsc2.jpg";
-import jmbj2 from "../assets/img/jmbj2.jpg";
-import pfbc2 from "../assets/img/pfbc2.jpg";
 import acm from "../assets/img/acm.jpg";
 import maa5 from "../assets/img/maa5.jpg";
 import tjas from "../assets/img/tjas.jpg";
@@ -9,38 +6,15 @@ import ffss from "../assets/img/ffss.jpeg";
 import dlc3 from "../assets/img/dlc3.jpeg";
 import gnmp from "../assets/img/gnmp.jpeg";
 import lsa4 from "../assets/img/lsa4.jpeg";
-import nrc2 from "../assets/img/nrc2.jpeg";
-import man2 from "../assets/img/man2.jpeg";
+import aam4 from "../assets/img/aam4.jpeg";
+import agaf2 from "../assets/img/agaf2.png";
+import ftm2 from "../assets/img/ftm2.png";
 
 const data = [
     {
-        name: "José Marcondes",
-        login: "jmbj2",
-        periodo: 5,
-        img: jmbj2,
-        linkedin: "",
-        github: "https://github.com/mallocaholic",
-    },
-    {
-        name: "Pedro Fernandes",
-        login: "pfbc2",
-        periodo: 5,
-        img: pfbc2,
-        linkedin: "",
-        github: "https://github.com/fernandesbarreto",
-    },
-    {
-        name: "Daniel Silva",
-        login: "jdsc2",
-        periodo: 5,
-        img: jdsc2,
-        linkedin: "",
-        github: "",
-    },
-    {
         name: "Marcela Asfora",
         login: "maa5",
-        periodo: 4,
+        periodo: 5,
         img: maa5,
         linkedin: "",
         github: "https://github.com/maa134340",
@@ -48,31 +22,15 @@ const data = [
     {
         name: "Thiago Alves",
         login: "tjas",
-        periodo: 4,
+        periodo: 5,
         img: tjas,
         linkedin: "https://www.linkedin.com/in/thijalves/",
         github: "https://github.com/Thijalves",
     },
     {
-        name: "Nathalia Coelho",
-        login: "nrc2",
-        periodo: 3,
-        img: nrc2,
-        linkedin: "https://www.linkedin.com/in/nath%C3%A1lia-rezende-coelho-a2a2781ba/",
-        github: "https://github.com/nrc2",
-    },
-    {
-        name: "Mateus Neves",
-        login: "man2",
-        periodo: 3,
-        img: man2,
-        linkedin: "",
-        github: "https://github.com/MateusNeeves",
-    },
-    {
         name: "Ana Laura",
         login: "alas4",
-        periodo: 2,
+        periodo: 3,
         img: alas4,
         linkedin: "https://www.linkedin.com/in/ana-laura-albuquerque-616365258",
         github: "https://github.com/anaalbsantos",
@@ -80,7 +38,7 @@ const data = [
     {
         name: "Danilo Carvalho",
         login: "dlc3",
-        periodo: 2,
+        periodo: 3,
         img: dlc3,
         linkedin: "",
         github: "https://github.com/DaniloLDCarvalho",
@@ -88,7 +46,7 @@ const data = [
     {
         name: "Felipe santos",
         login: "ffss",
-        periodo: 2,
+        periodo: 3,
         img: ffss,
         linkedin: "https://www.linkedin.com/in/felipe-ferreira-da-silva-santos-15aa4624a/",
         github: "https://github.com/SageScroll18144",
@@ -96,7 +54,7 @@ const data = [
     {
         name: "Gabriel Nogueira",
         login: "gnmp",
-        periodo: 2,
+        periodo: 3,
         img: gnmp,
         linkedin: "https://www.linkedin.com/in/gabriel-nogueira-a4849424b/",
         github: "https://github.com/nogueira04",
@@ -104,11 +62,38 @@ const data = [
     {
         name: "Lucas Absalão",
         login: "lsa4",
-        periodo: 2,
+        periodo: 3,
         img: lsa4,
         linkedin: "https://www.linkedin.com/in/lucas-absalão-66144522a/",
         github: "https://github.com/LightAsh04",
     },
+    {
+        name: "Arthur Marsaro",
+        login: "aam4",
+        periodo: 3,
+        img: aam4,
+        linkedin: "https://www.linkedin.com/in/arthur-marsaro-87a486230",
+        github: "",
+    },
+    {
+        name: "Alberto Guevara",
+        login: "agaf2",
+        periodo: 2,
+        img: agaf2,
+        linkedin: "https://www.linkedin.com/in/alberto-guevara-de-ara%C3%BAjo-franca-010885233",
+        github: "https://github.com/agaf2",
+    },
+    {
+        name: "Felipe Torres",
+        login: "ftm2",
+        periodo: 2,
+        img: ftm2,
+        linkedin: "https://www.linkedin.com/in/felipe-torres-334642236",
+        github: "https://github.com/ftm2-cin",
+    },
+  
+  
+
     
 ];
 
